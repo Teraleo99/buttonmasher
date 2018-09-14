9ffec35bd624dd4c9630480b01ba4e8b7b3adb69
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2018-09-14 15:24:56 +0100

    Score decimals + negatives fixed

--- Assets/Scripts/buttonMasher.cs
+++ Assets/Scripts/buttonMasher.cs
@@ -12,6 +12,7 @@ public class buttonMasher : MonoBehaviour {
 
     private int score = 0; // The numerical data value of our score
     private float timeRemaining = 0; // Numerical time remaining
+    private bool gameRunning = true; 
 
 	// Use this for initialization
 	void Start () {
@@ -32,6 +33,16 @@ public class buttonMasher : MonoBehaviour {
 
         timerText.text = (Mathf.CeilToInt(timeRemaining)).ToString();
 
+        // Check if we have run out of time
+        if (timeRemaining <= 0)
+        {
+            // Stop the game running
+            gameRunning = false;
+
+            // Stops time from going negative
+            timeRemaining = 0;
+        } // End of If (timeRunning)
+
     }// End of Update()
 
     // Responds to event from Unity that the object has been clicked 
@@ -39,12 +50,18 @@ public class buttonMasher : MonoBehaviour {
     void OnMouseDown()
     {
         Debug.Log("OnMouseDown method called");
-        // Trigger our clicking sound
-        clickSound.Play();
-        // Increase score by 1
-        score = score + 1;
-        // Update visual score
-        scoreText.text = score.ToString();
+
+        // Check if the game is still running
+        if (gameRunning == true)
+        {
+
+            // Trigger our clicking sound
+            clickSound.Play();
+            // Increase score by 1
+            score = score + 1;
+            // Update visual score
+            scoreText.text = score.ToString();
+        } // End of If (gameRunning = true)
     } // End of OnMouseDown()
 
 
